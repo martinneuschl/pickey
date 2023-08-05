1a8e364f8e1914006dd304f641d1536dc0e9d7ad
Martin Neuschl <martin.neuschl1@gmail.com>
2023-08-05 21:13:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
+# streamlit_app.py
+
 import streamlit as st
-import pandas as pd
+from google.oauth2 import service_account
+from google.cloud import bigquery
 
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
 
-header = st.container()
-dataset = st.container()
-features = st.container()
-model_training = st.container()
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
 
-with header:
-	st.title('Pickey.cz')
-	st.markdown('Analytika pro tvůrce - proof-of-concept. Cílem tohoto dema je ukázat interaktivní reporting, který ukazuje data z Google Analytics 4. Každý tvůrce bude mít náhled pouze na svá data a má tak možnost získat okamžitý přehled o výkonu svého profilu na Pickey.')
+rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
 
-with dataset:
-	st.header('Reporting pro tvůrce')
-	st.markdown('Tohle je ukázka JavaScript snippetu:')
-	st.code('console.log("Ahoj!")')
-
-	taxi_data = pd.read_csv('')
-with features:
-	st.header('Základní data')
-
-#with model_training:
+# Print results.
+st.write("Some wise words from Shakespeare:")
+for row in rows:
+    st.write("✍️ " + row['word'])
